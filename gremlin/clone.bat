235c81e9b13828260f38968f422eb7d3b343dc01
xiaodoli <xiaodoli@microsoft.com>
2019-02-04 15:39:13 -0800

    "submodule"

--- gremlin/clone.bat
+++ gremlin/clone.bat
@@ -5,5 +5,4 @@ git submodule add https://github.com/Azure-Samples/azure-cosmos-db-graph-gremlin
 git submodule add https://github.com/FlorianHockmann/Gremlin.Net.git
 git submodule add https://github.com/boonzaai/GremlinQuery.net.git
 git submodule add https://github.com/evo-terren/Gremlin.Net.CosmosDb.git
-git submodule add https://github.com/ExRam/ExRam.Gremlinq.git
 git submodule add https://github.com/inthefabric/Weaver.git 
