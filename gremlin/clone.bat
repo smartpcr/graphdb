d3bbc7b4c7732f9e77d622e4004dc4132d9d50f4
lingxd <lingxd@gmail.com>
2019-04-01 08:55:03 -0700

    chmod clone

--- gremlin/clone.bat
+++ gremlin/clone.bat
@@ -1,8 +1,8 @@
-git submodule add https://github.com/Azure-Samples/azure-cosmosdb-graph-bulkexecutor-dotnet-getting-started.git 
-git submodule add https://github.com/crosscutsolutions/gremlin.linq.git
-git submodule add https://github.com/ExRam/ExRam.Gremlinq.git
-git submodule add https://github.com/Azure-Samples/azure-cosmos-db-graph-gremlindotnet-getting-started.git
-git submodule add https://github.com/FlorianHockmann/Gremlin.Net.git
-git submodule add https://github.com/boonzaai/GremlinQuery.net.git
-git submodule add https://github.com/evo-terren/Gremlin.Net.CosmosDb.git
-git submodule add https://github.com/inthefabric/Weaver.git 
+git clone https://github.com/Azure-Samples/azure-cosmosdb-graph-bulkexecutor-dotnet-getting-started.git 
+git clone https://github.com/crosscutsolutions/gremlin.linq.git
+git clone https://github.com/ExRam/ExRam.Gremlinq.git
+git clone https://github.com/Azure-Samples/azure-cosmos-db-graph-gremlindotnet-getting-started.git
+git clone https://github.com/FlorianHockmann/Gremlin.Net.git
+git clone https://github.com/boonzaai/GremlinQuery.net.git
+git clone https://github.com/evo-terren/Gremlin.Net.CosmosDb.git
+git clone https://github.com/inthefabric/Weaver.git 
